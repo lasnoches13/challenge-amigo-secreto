d2615cdef5a668079a8dd656e7c327b67c915f27
Thiago Fontes <lasnoches13@gmail.com>
2025-02-25 21:06:56 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,34 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+//função para botão adicionar amigo
+function adicionarAmigo(){
+let nomeAmigo = document.querySelector('input').value;
+if (nomeAmigo == ''){
+    alert('Por favor, insira um nome.')
+}else{
+listaAmigos.push(nomeAmigo);
+console.log(listaAmigos);
+mostrarAmigo();
+limparInput();
+}}
+//mostrar lista de amigos adicionados
+function mostrarAmigo(){
+    let campo = document.querySelector('ul');
+        campo.innerHTML = listaAmigos;
+    }
+
+//função limpar input
+function limparInput(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+//função sortear amigo
+function sortearAmigo(){
+let amigoSorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
+alert(`O seu amigo secreto é: ${amigoSorteado}`);
+listaAmigos =[];
+limparInput();
+return amigoSorteado;
+}
+
