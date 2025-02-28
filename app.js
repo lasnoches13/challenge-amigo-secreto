f856abeb0f319169a7d31ea145fc598c2abd9c0c
Thiago Fontes <lasnoches13@gmail.com>
2025-02-28 10:44:55 -0300

    app funcionando 100%

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let listaAmigos = [];
+let Amigos = [];
 
-//função para botão adicionar amigo
+//1.função para botão adicionar amigo
 function adicionarAmigo(){
-let nomeAmigo = document.querySelector('input').value;
+let inputAmigo = document.getElementById('amigo');
+let nomeAmigo = inputAmigo.value;
 if (nomeAmigo == ''){ // caso nome seja vazio, mostrar alert.
     alert('Por favor, insira um nome.');
-    limparInput();
-}else if(listaAmigos.includes(nomeAmigo)){ //caso a lista ja contenha um nome informado, mostra outro alert
+}else if(Amigos.includes(nomeAmigo)){ //caso a lista ja contenha um nome informado, mostra outro alert
     alert('Nome ja existente, por favor insira um nome diferente.');
     limparInput();
 }else{
-listaAmigos.push(nomeAmigo);
-console.log(listaAmigos);
-mostrarAmigo();
+Amigos.push(nomeAmigo);
+console.log(Amigos);
 limparInput();
+inputAmigo.focus();
+mostrarAmigosNoHtml();
+
 }}
-//mostrar lista de amigos adicionados
-function mostrarAmigo(){
-    let campo = document.querySelector('ul');
-        campo.innerHTML = listaAmigos;
-    }
-//função limpar input
+//2.mostrar lista de amigos adicionados no html
+function mostrarAmigosNoHtml(){
+    let amigosHtml = document.getElementById('listaAmigos');
+    amigosHtml.innerHTML = "";
+
+    for(let i=0; i < Amigos.length; i++){
+        let item = document.createElement("li");
+        item.textContent = Amigos[i];
+        amigosHtml.appendChild(item);
+}
+}
+//3.função limpar input
 function limparInput(){
     nomeAmigo = document.querySelector('input');
     nomeAmigo.value = '';
 }
-//função sortear amigo
+//4.função sortear amigo
 function sortearAmigo(){
-let amigoSorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
-alert(`O seu amigo secreto é: ${amigoSorteado}`);
-limparInput();
-listaAmigos = [];
-mostrarAmigo();
-return amigoSorteado;
+    if (Amigos.length === 0){
+        alert('Nenhum amigo adicionado');
+        return;
+    }
+let amigoSorteado = Amigos[Math.floor(Math.random() * Amigos.length)];
+let resultado = document.getElementById('resultado');
+resultado.innerHTML = `O amigo sorteado foi: ${amigoSorteado}`;
+let limparLista = document.getElementById('listaAmigos');
+limparLista.innerHTML = "";
 }
 
